d03c09e39046754f5374a32fb36781bda1aed6bb
USER <USER@10.20.100.118>
2021-03-05 16:24:09 +0530

    First commit

---
+++ DemoGithubApp/src/com/faith/com/DemoApp.java
@@ -0,0 +1,11 @@
+package com.faith.com;
+
+public class DemoApp {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		System.out.println("welcome to github");
+	}
+
+}
